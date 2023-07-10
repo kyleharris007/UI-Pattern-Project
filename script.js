b32651d8b4ed994ac99f8b0475923bb2bfbbe63b
Kyle Harris <135283781+kyleharris007@users.noreply.github.com>
2023-07-10 10:31:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+let characters = []
+const charBtn = document.querySelector("#characters")
+const jsCharacters = document.querySelector("#HTMLCharacters");
 
+function rickAndMortyAPI() {
+
+    let url = 'https://rickandmortyapi.com/api/character'
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data)
+                // Clear HTML Contents and Characters array
+                jsCharacters.innerHTML = ""
+                characters.length = 0
+                
+                for (let i = 0; i < data.results.length; i++) {
+                    if (data.results[i].name === "Rick Sanchez" || data.results[i].name === "Morty Smith" || data.results[i].name === "Amish Cyborg" || data.results[i].name === "Summer Smith" || data.results[i].name === "Jerry Smith") {
+                        characters.push(data.results[i])
+                    }
+                }
+
+                characters.forEach(char => {
+                    // let p = document.createElement("p")
+                    // p.innerHTML = char.name
+                    // jsCharacters.appendChild(p)
+                    let charHTML = `
+                        <div class="character-container">
+                            <p>${char.name}</p>
+                        </div>
+                    `
+
+                    jsCharacters.insertAdjacentHTML("beforeend", charHTML)
+                })
+            })
+}
+
+// rickAndMortyAPI();
+
+
+charBtn.addEventListener("click", rickAndMortyAPI)
